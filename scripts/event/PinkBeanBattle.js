b257eb65539c7bc2a273aead943ebfd38137e1c3
geermanperez <46948035+geermanperez@users.noreply.github.com>
2026-06-06 04:32:50 -0300

    penk bean

--- scripts/event/PinkBeanBattle.js
+++ scripts/event/PinkBeanBattle.js
@@ -35,7 +35,7 @@ var minMapId = 270050100;
 var maxMapId = 270050300;
 
 var eventTime = 140;     // 140 minutes
-var clearExpReward = 1000000000000;
+var clearExpReward = 100000000;
 var maxExpChunk = 2147483647;
 
 const maxLobbies = 1;
