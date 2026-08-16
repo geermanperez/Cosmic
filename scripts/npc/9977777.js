84ec76039c2c1bbca6a73230ea350dafb8eef432
geermanperez <germanpereznieva@gmail.com>
2026-08-16 13:10:56 -0300

    update

--- scripts/npc/9977777.js
+++ scripts/npc/9977777.js
@@ -1,9 +1,9 @@
 /*
     NPC de prueba desactivado.
-    El EVENTO DIA DEL AMIGO se cobra desde Mimi, NPC 9209001.
+    El Evento Dia del Amigo finalizo.
  */
 
 function start() {
-    cm.sendOk("Este NPC de prueba ya no entrega recompensas.\r\n\r\nEl #eEVENTO DIA DEL AMIGO#n ahora se cobra con #bMimi#k, NPC #b9209001#k.");
+    cm.sendOk("El evento concluyo. Gracias por participar.");
     cm.dispose();
 }
