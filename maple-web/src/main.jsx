00fdd8c60208bcdce4d298e1121f69c29acb1958
geermanperez <46948035+geermanperez@users.noreply.github.com>
2026-08-31 13:35:26 -0300

    brand: activate EverleafMs theme and runtime identity

--- maple-web/src/main.jsx
+++ maple-web/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './brand.css'
 import App from './App.jsx'
+import { startEverleafBranding } from './brandRuntime.js'
+
+startEverleafBranding()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
